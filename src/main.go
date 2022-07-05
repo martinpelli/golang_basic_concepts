62f0f71f4ef3048bd26d43570c58e29893917f44
pelli <martinpelli@outlook.com>
2022-07-05 09:47:18 -0300

    datos primitivos

--- src/main.go
+++ src/main.go
@@ -36,4 +36,56 @@ func main() {
 
 	fmt.Println(areaCuadrado)
 
+	//Operadores aritméticos
+
+	x := 10
+	y := 50
+
+	result := x + y
+
+	fmt.Println(result)
+
+	const radio float64 = 5
+	const h float64 = 4.43
+	const z float64 = 3
+	const w float64 = 5
+
+	areaCirculo := pi * radio * h
+	areaRectangulo := radio * h
+	areaTrapecio := h * ((z + w) / 2)
+
+	fmt.Println(areaCirculo, areaRectangulo, areaTrapecio)
+
+	//Incremental
+	x++
+	//Decremental
+	x--
+
+	//Numeros enteros
+	//int = Depende del OS (32 o 64 bits)
+	//int8 = 8bits = -128 a 127
+	//int16 = 16bits = -2^15 a 2^15-1
+	//int32 = 32bits = -2^31 a 2^31-1
+	//int64 = 64bits = -2^63 a 2^63-1
+
+	//Optimizar memoria cuando sabemos que el dato simpre va ser positivo
+	//uint = Depende del OS (32 o 64 bits)
+	//uint8 = 8bits = 0 a 127
+	//uint16 = 16bits = 0 a 2^15-1
+	//uint32 = 32bits = 0 a 2^31-1
+	//uint64 = 64bits = 0 a 2^63-1
+
+	//numeros decimales
+	// float32 = 32 bits = +/- 1.18e^-38 +/- -3.4e^38
+	// float64 = 64 bits = +/- 2.23e^-308 +/- -1.8e^308
+
+	//textos y booleanos
+	//string = ""
+	//bool = true or false
+
+	//numeros complejos
+	//Complex64 = Real e Imaginario float32
+	//Complex128 = Real e Imaginario float64
+	//Ejemplo : c:=10 + 8i
+
 }
